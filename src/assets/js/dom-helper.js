28c49f31eafe2d8ba7e94fdf32a8ae2dfd1f5caf
I Wayan Vendy Wiranatha <vendywira@gmail.com>
2019-03-31 14:47:40 +0700

    create shorthand to dom manipulation

---
+++ src/assets/js/dom-helper.js
@@ -0,0 +1,60 @@
+/**
+ * @author vendywira
+ * this helper will help you to simplifiying dom manipulation
+ * this helper work like jquery
+ * import this library on your code and create class instant
+ */
+
+class DOMHelper {
+
+  constructor() {
+    this._documentSelector = null
+  }
+
+  document(querySelector) {
+    this._documentSelector = document.querySelector(querySelector)
+    return this
+  }
+
+  click(func) {
+    this._documentSelector.addEventListener('click', func)
+  }
+
+  dblClick(func) {
+    this._documentSelector.addEventListener('dblclick', func)
+  }
+
+  mouseOver(func) {
+    this._documentSelector.addEventListener('mouseover', func)
+  }
+
+  mouseEnter(func) {
+    this._documentSelector.addEventListener('mouseenter', func)
+  }
+
+  mouseLeave(func) {
+    this._documentSelector.addEventListener('mouseleave', func)
+  }
+
+  mouseDown(func) {
+    this._documentSelector.addEventListener('mouseleave', func)
+  }
+
+  change(func) {
+    this._documentSelector.addEventListener('change', func)
+  }
+
+  keyUp(func) {
+    this._documentSelector.addEventListener('keyup', func)
+  }
+
+  keyDown(func) {
+    this._documentSelector.addEventListener('keydown', func)
+  }
+
+  keyPress(func) {
+    this._documentSelector.addEventListener('keypress', func)
+  }
+}
+
+export default DOMHelper;
