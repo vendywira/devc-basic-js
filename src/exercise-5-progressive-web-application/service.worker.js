baa865d082fa34df76c4e789a6f6f4c581477a81
I Wayan Vendy Wiranatha <vendywira@gmail.com>
2019-05-04 03:28:21 +0700

    change cache mecanishm

--- src/exercise-5-progressive-web-application/service.worker.js
+++ src/exercise-5-progressive-web-application/service.worker.js
@@ -44,38 +44,31 @@
  /*
    FETCH EVENT: triggered for every request made by index page, after install.
  */
-
- //Adding `fetch` event listener
  self.addEventListener('fetch', event => {
      console.info('Event: Fetch');
 
-     var request = event.request;
+     let request = event.request;
 
-     //Tell the browser to wait for newtwork request and respond with below
      event.respondWith(
-         //If request is already in cache, return it
          caches.match(request).then((response) => {
              if (response) {
                  return response;
              }
 
-             // // Checking for navigation preload response
-             // if (event.preloadResponse) {
-             //   console.info('Using navigation preload');
-             //   return response;
-             // }
-
-             //if request is not cached or navigation preload response, add it to cache
-             return fetch(request).then((response) => {
-                 var responseToCache = response.clone();
-                 caches.open(cacheName).then((cache) => {
-                     cache.put(request, responseToCache).catch((err) => {
-                         console.warn(request.url + ': ' + err.message);
+             if (navigator.onLine) {
+                 return fetch(request).then((response) => {
+                     let responseToCache = response.clone();
+                     caches.open(cacheName).then((cache) => {
+                         console.log(request, responseToCache);
+                         if (!caches.has(request)) {
+                             cache.add(request, responseToCache)
+                         } else {
+                             cache.put(request, responseToCache)
+                         }
                      });
+                     return response;
                  });
-
-                 return response;
-             });
+             }
          })
      );
  });
